30cd78918a6024d0421bdc2bc40a12403ae40ab9
Hoodini2117 <ayaan171104@gmail.com>
2026-02-28 23:51:58 +0530

    fix

--- ecommerce_sanity_stripe-main/lib/utils.js
+++ ecommerce_sanity_stripe-main/lib/utils.js
@@ -1,24 +1,38 @@
-import confetti from 'canvas-confetti';
-
 export const runFireworks = () => {
-  var duration = 5 * 1000;
-  var animationEnd = Date.now() + duration;
-  var defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
+  if (typeof window === 'undefined') return;
 
-  function randomInRange(min, max) {
-    return Math.random() * (max - min) + min;
-  }
+  import('canvas-confetti').then((confettiModule) => {
+    const confetti = confettiModule.default;
 
-  var interval = setInterval(function() {
-    var timeLeft = animationEnd - Date.now();
+    var duration = 5 * 1000;
+    var animationEnd = Date.now() + duration;
+    var defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
 
-    if (timeLeft <= 0) {
-      return clearInterval(interval);
+    function randomInRange(min, max) {
+      return Math.random() * (max - min) + min;
     }
 
-    var particleCount = 50 * (timeLeft / duration);
-    // since particles fall down, start a bit higher than random
-    confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 } }));
-    confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 } }));
-  }, 250);
-}
+    var interval = setInterval(function () {
+      var timeLeft = animationEnd - Date.now();
+
+      if (timeLeft <= 0) {
+        return clearInterval(interval);
+      }
+
+      var particleCount = 50 * (timeLeft / duration);
+
+      confetti({
+        ...defaults,
+        particleCount,
+        origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 }
+      });
+
+      confetti({
+        ...defaults,
+        particleCount,
+        origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 }
+      });
+
+    }, 250);
+  });
+};
