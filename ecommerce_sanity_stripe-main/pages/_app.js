b7baca077ddafa220eb4af97291114e2de76e84f
Hoodini2117 <ayaan171104@gmail.com>
2026-02-28 23:58:12 +0530

    debug

--- ecommerce_sanity_stripe-main/pages/_app.js
+++ ecommerce_sanity_stripe-main/pages/_app.js
@@ -1,19 +1,3 @@
-import React from 'react';
-import { Toaster } from 'react-hot-toast';
-
-import { Layout } from '../components';
-import '../styles/globals.css';
-import { StateContext } from '../context/StateContext';
-
-function MyApp({ Component, pageProps }) {
-  return (
-    <StateContext>
-      <Layout>
-        <Toaster />
-        <Component {...pageProps} />
-      </Layout>
-    </StateContext>
-  )
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
 }
-
-export default MyApp
